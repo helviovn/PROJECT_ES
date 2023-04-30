6d70a71032f1c6cf237ec89b84121a95050fb0a5
[afonsopacheco50] <[afonsopacheco50@hotmail.com]>
2023-04-30 18:24:35 +0100

    Criei classe competition

--- PROJECT_ES/Data/Migrations/dbo.Competition.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Table]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(255) NOT NULL, 
-    [data_inicio] DATETIME NOT NULL, 
-    [data_fim] DATETIME NOT NULL, 
-    [n_participantes] INT NULL 
-)
--- PROJECT_ES/Data/Migrations/dbo.Competiton.sql
+++
@@ -1,8 +0,0 @@
-﻿CREATE TABLE [dbo].[Competition]
-(
-	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
-    [Name] VARCHAR(255) NOT NULL, 
-    [data_inicio] DATETIME NOT NULL, 
-    [data_fim] DATETIME NOT NULL, 
-    [n_participantes] INT NULL 
-)
