d48d69511254d071eccc11796cfc2bf8719a8093
[afonsopacheco50] <[afonsopacheco50@hotmail.com]>
2023-04-30 18:35:24 +0100

    Meti a Competition na migration

---
+++ PROJECT_ES/Data/Migrations/20230430173441_MigrationComp.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PROJECTES.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class MigrationComp : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
