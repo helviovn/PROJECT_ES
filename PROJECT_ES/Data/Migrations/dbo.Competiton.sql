189fdce3a8e765c501212de1c4e056fb60a144e8
[afonsopacheco50] <[afonsopacheco50@hotmail.com]>
2023-04-30 15:53:04 +0100

    Tentei meter tabela nas migrations

---
+++ PROJECT_ES/Data/Migrations/dbo.Competiton.sql
@@ -0,0 +1,8 @@
+﻿CREATE TABLE [dbo].[Competition]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [Name] VARCHAR(255) NOT NULL, 
+    [data_inicio] DATETIME NOT NULL, 
+    [data_fim] DATETIME NOT NULL, 
+    [n_participantes] INT NULL 
+)
