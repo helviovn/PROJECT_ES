7a5f69d47494407b005109bc718bb9ded844a234
pedroandrade4059 <pedrogsandrade4059@gmail.com>
2023-05-07 01:06:25 +0100

    feat : categories page

---
+++ PROJECT_ES/Service/CategoryRepository.cs
@@ -0,0 +1,85 @@
+
+
+using Dapper;
+using Microsoft.Data.SqlClient;
+using PROJECT_ES.Data;
+
+namespace PROJECT_ES.Service;
+
+public class CategoryRepository
+{
+    private readonly string _connectionString;
+
+    public CategoryRepository(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+    
+    public async Task AddCategoryAsync(String name)
+    {
+        //query para inserir na base de dados as categorias
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+
+            var query = "INSERT INTO dbo.Category (Name) " +
+                        "VALUES (@Name);" +
+                        "SELECT CAST(SCOPE_IDENTITY() as int)";
+
+            var parameters = new
+            {
+                Name = name
+            };
+
+            var id = await connection.ExecuteScalarAsync<int>(query,parameters);
+
+            
+        }
+    }
+    
+    public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+
+            var query = "SELECT * FROM dbo.Category";
+        
+            var categories = await connection.QueryAsync<Category>(query);
+
+            return categories;
+        }
+    }
+    
+    public async Task DeleteMovieAsync(int id)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+
+            var query = "DELETE FROM dbo.Category WHERE Id = @Id";
+
+            var parameters = new { Id = id };
+
+            await connection.ExecuteAsync(query, parameters);
+        }
+    }
+    
+    public async Task UpdateCategoryAsync(int id, string newName)
+    {
+        using (var connection = new SqlConnection(_connectionString))
+        {
+            await connection.OpenAsync();
+        
+            var query = "UPDATE dbo.Category SET Name = @Name WHERE Id = @Id";
+        
+            await connection.ExecuteAsync(query, new { Id = id, Name = newName });
+        }
+    }
+
+
+
+    
+    
+    
+}
