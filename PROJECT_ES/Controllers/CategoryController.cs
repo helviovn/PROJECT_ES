109bb34b397537c0970e176cacfac7010400e1b9
[afonsopacheco50] <[afonsopacheco50@hotmail.com]>
2023-05-30 22:12:20 +0100

    feat: Factory Method

--- PROJECT_ES/Controllers/CategoryController.cs
+++ PROJECT_ES/Controllers/CategoryController.cs
@@ -2,33 +2,36 @@
 using Microsoft.AspNetCore.Mvc;
 using PROJECT_ES.Service;
 using PROJECT_ES.ViewModels;
+using PROJECT_ES.Interfaces;
+
 public class CategoryController : Controller
 {
-    private readonly CompetitionRepository _competitionRepository;
+    private readonly ICompetitionRepositoryFactory _competitionRepositoryFactory;
     private readonly CompetitionDetailsRepository _competitionDetailsRepository;
+    private readonly IConfiguration _connectionString;
 
-    public CategoryController(CompetitionRepository competitionRepository, CompetitionDetailsRepository competitionDetailsRepository)
+    public CategoryController(ICompetitionRepositoryFactory competitionRepositoryFactory, CompetitionDetailsRepository competitionDetailsRepository, IConfiguration configuration)
     {
-        _competitionRepository = competitionRepository;
+        _competitionRepositoryFactory = competitionRepositoryFactory;
         _competitionDetailsRepository = competitionDetailsRepository;
+        _connectionString = configuration;
     }
 
     public async Task<IActionResult> CategoriesPage(int competitionId)
     {
-        var competition = await _competitionRepository.GetCompetitionByIdAsync(competitionId);
+        var competitionRepository = _competitionRepositoryFactory.Create(_connectionString);
+        
+        var competition = await competitionRepository.GetCompetitionByIdAsync(competitionId);
         var categories = await _competitionDetailsRepository.GetAllCategoriesAsync(competitionId);
         var movies = await _competitionDetailsRepository.GetAllMoviesAsync(competitionId);
-
 
         var viewModel = new CategoryViewModel
         {
             Competition = competition,
             Categories = categories,
-            Movies = movies,
-            
+            Movies = movies
         };
 
         return View("CategoriesPage", viewModel);
-
     }
 }
